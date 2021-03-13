029f2fe4f1fa120220db3976c9fa0640e8e7b74b
TJ-DHBW <tim-johmann@web.de>
2021-03-13 16:18:59 +0100

    extended database interface

--- src/main/java/msa/db/HSQLDatabase.java
+++ src/main/java/msa/db/HSQLDatabase.java
@@ -1,5 +1,110 @@
 package msa.db;
 
+import msa.db.model.Algorithm;
+import msa.db.model.Channel;
+import msa.db.model.Participant;
+import msa.db.model.Type;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import javax.persistence.NoResultException;
+import java.util.List;
+
 public class HSQLDatabase implements IMSADatabase {
-    //TODO implement
+    private final Session session;
+
+    public HSQLDatabase() {
+        this.session = HibernateUtil.getSessionFactory().openSession();
+    }
+
+    @Override
+    public void save(Object object) {
+        session.save(object);
+    }
+
+    @Override
+    public void delete(Object object) {
+        session.delete(object);
+    }
+
+    //TODO Test
+    @Override
+    public Algorithm findAlgorithmByName(String name) {
+        String queryString = "FROM Algorithm A WHERE A.name = :name";
+        Query<Algorithm> query = session.createQuery(queryString, Algorithm.class);
+        query.setParameter("name", name);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    //TODO Test
+    @Override
+    public Type findTypeByName(String name) {
+        String queryString = "FROM Type T WHERE T.name = :name";
+        Query<Type> query = session.createQuery(queryString, Type.class);
+        query.setParameter("name", name);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    //TODO Test
+    @Override
+    public Participant findParticipantByName(String name) {
+        String queryString = "FROM Participant P WHERE P.name = :name";
+        Query<Participant> query = session.createQuery(queryString, Participant.class);
+        query.setParameter("name", name);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    //TODO IMPORTANT Test
+    @Override
+    public List<Channel> findAllChannelWithParticipant(Participant participant) {
+        String queryString = "FROM Channel C WHERE C.participant_01 = :participant OR C.participant_02 = :participant";
+        Query<Channel> query = session.createQuery(queryString, Channel.class);
+        query.setParameter("participant", participant);
+        return query.list();
+    }
+
+    //TODO IMPORTANT Test
+    @Override
+    public Channel findChannelByParticipants(Participant participant1, Participant participant2) {
+        String queryString = "FROM Channel C WHERE (C.participant_01 = :participant1 AND C.participant_02 = :participant2) OR (C.participant_01 = :participant2 AND C.participant_02 = :participant1)";
+        Query<Channel> query = session.createQuery(queryString, Channel.class);
+        query.setParameter("participant1", participant1);
+        query.setParameter("participant2", participant2);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    //TODO Test
+    @Override
+    public Channel findChannelByName(String name) {
+        String queryString = "FROM Channel C WHERE C.name = :name";
+        Query<Channel> query = session.createQuery(queryString, Channel.class);
+        query.setParameter("name", name);
+        try {
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
+    }
+
+    //TODO Test
+    @Override
+    public List<Channel> getAllChannel() {
+        return session.createQuery("FROM Channel", Channel.class).list();
+    }
 }
--- src/main/java/msa/db/IMSADatabase.java
+++ src/main/java/msa/db/IMSADatabase.java
@@ -1,5 +1,30 @@
 package msa.db;
 
+import msa.db.model.Algorithm;
+import msa.db.model.Channel;
+import msa.db.model.Participant;
+import msa.db.model.Type;
+
+import java.util.List;
+
 public interface IMSADatabase {
     //TODO write some boring stuff in here.
+
+    void save(Object object);
+
+    void delete(Object object);
+
+    Algorithm findAlgorithmByName(String name);
+
+    Type findTypeByName(String name);
+
+    Participant findParticipantByName(String name);
+
+    List<Channel> findAllChannelWithParticipant(Participant participant);
+
+    Channel findChannelByParticipants(Participant participant1, Participant participant2);
+
+    Channel findChannelByName(String name);
+
+    List<Channel> getAllChannel();
 }
